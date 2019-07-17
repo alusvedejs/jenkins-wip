a8a903a22c0b53bf5c12ee2697121cf0c4b6fa75
sergejs.solomonovs <sergejs.solomonovs>
2019-07-17 12:43:25 +0300

    wip

--- jobs/seed_all.groovy
+++ jobs/seed_all.groovy
@@ -1,6 +1,6 @@
 job('Seed All') {
   scm {
-    git ('https://github.com/alusvedejs/jenkins-wip.git')
+    git ('https://github.com/alusvedejs/jenkins-wip.git',{node -> node / 'extensions' << '' })
   }
   steps {
     dsl {
@@ -11,3 +11,33 @@ job('Seed All') {
     }
   }
 }
+
+
+
+// git('https://github.com/demo/demo.git','development',{node -> node / 'extensions' << '' })
+
+
+
+// pipelineJob('DSL_Pipeline') {
+
+//   def repo = 'https://github.com/path/to/your/repo.git'
+
+//   triggers {
+//     scm('H/5 * * * *')
+//   }
+//   description("Pipeline for $repo")
+
+//   definition {
+//     cpsScm {
+//       scm {
+//         git {
+//           remote { url(repo) }
+//           branches('master', '**/feature*')
+//           scriptPath('misc/Jenkinsfile.v2')
+//           extensions { }
+//         }
+
+//       }
+//     }
+//   }
+// }
